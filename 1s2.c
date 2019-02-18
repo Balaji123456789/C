2938e1103bcac9e3e899f2606c62b7d8beb6c2da
Balaji123456789 <45650405+Balaji123456789@users.noreply.github.com>
2019-02-18 16:16:04 +0530

    Update and rename 1s2 to 1s2.c

---
+++ 1s2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int num, rem;
+ 
+    printf("Input an integer : ");
+    scanf("%d", &num);
+    rem1 = num % 2;
+    if (rem == 0)
+        printf("%d is an even integer\n", num);
+    if (rem!=0)
+        printf("%d is an odd integer\n", num);
+        else
+        printf("invalid");
+        
+        
+}
