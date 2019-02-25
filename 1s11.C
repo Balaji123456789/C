5ab60b8c8e7e9ad52c1d71d42fd74b4b9724e9b1
Balaji123456789 <45650405+Balaji123456789@users.noreply.github.com>
2019-02-25 16:10:15 +0530

    Create 1s11.C

---
+++ 1s11.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+	int m,e,p;
+	printf("Enter element and power:\n");
+	scanf("%d%d",&m,&e);
+    p=pow(m,e);
+
+	
+  printf("P is %d",power);
+}
