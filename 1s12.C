a17c98f089a2842934eabe02df9fddbb4e550f92
Balaji123456789 <45650405+Balaji123456789@users.noreply.github.com>
2019-02-25 16:14:55 +0530

    Create 1s12.C

---
+++ 1s12.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,d=0,sum=0,num;
+printf("\n Enter number");
+scanf("%d",&num);
+n=num;
+while(n!=0)
+{
+d=n%10;
+sum=sum*10+d;
+n=n/10;
+}
+printf("\n %d",sum);
+if(num==sum)
+{
+printf("\n Palindrome");
+}
+else
+{
+printf("\n Not Palindrome");
+}
+getch();
+}
