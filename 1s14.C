48d27a7c44164ddbdc1bd32b1046c5d9cca95fe9
Balaji123456789 <45650405+Balaji123456789@users.noreply.github.com>
2019-02-25 16:20:33 +0530

    Create 1s14.C

---
+++ 1s14.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+int main(void) {
+	long int c,d;
+	scanf("%ld %ld",&c,&d);
+	if(a<100000&&b<100000)
+	{
+	long int i;
+	for(i=c+1;i<=d;i++)
+	{
+		if(i%2!=0)
+		printf("%ld ",i);
+	}
+	}
+	else
+	
+	getch();
+}
