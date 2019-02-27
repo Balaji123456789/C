78032420b93b984b5d78ceb8eca10bf11425ddad
Balaji123456789 <45650405+Balaji123456789@users.noreply.github.com>
2019-02-27 16:07:55 +0530

    Create 1s25.C

---
+++ 1s25.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the number of elements");
+scanf("%d",&n);
+if(1<=n<=100)
+{
+printf("median element");
+}
+else
+{
+getch();
+}
+}
